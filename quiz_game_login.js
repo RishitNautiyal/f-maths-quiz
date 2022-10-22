b549c784492702a8b77a603a90cafdec36e816b5
RishitNautiyal <105141347+RishitNautiyal@users.noreply.github.com>
2022-10-22 11:04:22 +0530

    Add files via upload

---
+++ quiz_game_login.js
@@ -0,0 +1,10 @@
+
+function addUser(){
+    var player1_name = document.getElementById("player1_name_input").value;
+    var player2_name= document.getElementById("player2_name_input").value;
+
+    localStorage.setItem("name1", player1_name );
+    localStorage.setItem("name2",player2_name );
+
+    window.location= "game_page.html";
+}
